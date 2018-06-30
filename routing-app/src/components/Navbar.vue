c242a1497544adcc333a5d371d10a5212aef1deb
ruchiramore19 <ruchiramore19@gmail.com>
2018-07-01 01:01:20 +0530

    routing lessons

---
+++ routing-app/src/components/Navbar.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="main-nav">
+        <ul>
+            <li><router-link :to ="{name : 'About'}">About</router-link></li>
+            <li><router-link :to ="{name : 'Home'}">Home</router-link></li>
+
+        </ul>
+        <ul v-for="(id, index) in userIds" :key="index">
+            <router-link :to ="{name:'Profile', params:{user_id: id}}">Profile {{id}}</router-link>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'Navbar',
+        data(){
+            return{
+                userIds : ['1','2','3','4']
+            }
+        }
+    }
+</script>
+
+<style>
+ul{
+    list-style-type : none;
+    padding: 0;
+    display: flex;
+    flex-flow: row
+    }
+
+li{
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+    font-size: 25px;
+    padding: 20px;
+
+    
+}
+
+</style>
---
+++ routing-app/src/components/Profile.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="profile">
+        <h1> Profile {{userID}}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : 'Profile',
+        data(){
+            return{
+                userID : this.$route.params.user_id
+            }
+        },
+        methods : {
+            updateID (){
+                this.userID = this.$route.params.user_id
+            }
+        },
+        watch : {
+            $route : 'updateID'
+        }
+    }
+</script>
